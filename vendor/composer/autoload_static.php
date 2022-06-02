409528f6a49fe8f0c47ba3dd63fffec2fc3df0d3
Mark Myers <marcusmyers@gmail.com>
2022-06-02 10:11:48 -0400

    update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,7 +8,6 @@ class ComposerStaticInit78d28fc0e483873db15e209d90aa524b
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'fe62ba7e10580d903cc46d808b5961a4' => __DIR__ . '/..' . '/tightenco/collect/src/Collect/Support/helpers.php',
         'caf31cc6ec7cf2241cb6f12c226c3846' => __DIR__ . '/..' . '/tightenco/collect/src/Collect/Support/alias.php',
@@ -21,7 +20,6 @@ class ComposerStaticInit78d28fc0e483873db15e209d90aa524b
         ),
         'S' => 
         array (
-            'Symfony\\Polyfill\\Php72\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\VarDumper\\' => 28,
         ),
@@ -31,10 +29,6 @@ class ComposerStaticInit78d28fc0e483873db15e209d90aa524b
         'Tightenco\\Collect\\' => 
         array (
             0 => __DIR__ . '/..' . '/tightenco/collect/src/Collect',
-        ),
-        'Symfony\\Polyfill\\Php72\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-php72',
         ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
@@ -46,11 +40,16 @@ class ComposerStaticInit78d28fc0e483873db15e209d90aa524b
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit78d28fc0e483873db15e209d90aa524b::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit78d28fc0e483873db15e209d90aa524b::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit78d28fc0e483873db15e209d90aa524b::$classMap;
 
         }, null, ClassLoader::class);
     }
